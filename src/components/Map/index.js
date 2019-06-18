d57ec55130bbb0902c0fe8cd6d7e5390baab2206
Douglas Porto <douglasalexandre7@gmail.com>
2019-06-18 19:00:54 -0300

    Marker Distanation

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { View } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import { getPixelSize } from '../../utils';
 
 import Search from '../Search';
 import Directions from '../Directions';
+
+import markerImage from '../../assets/marker.png';
 export default class Map extends Component {
   state = {
     region: null,
@@ -54,20 +56,27 @@ export default class Map extends Component {
           ref={el => (this.mapView = el)}
         >
           {destination && (
-            <Directions
-              origin={region}
-              destination={destination}
-              onReady={result => {
-                this.mapView.fitToCoordinates(result.coordinates, {
-                  edgePadding: {
-                    right: getPixelSize(50),
-                    left: getPixelSize(50),
-                    top: getPixelSize(50),
-                    bottom: getPixelSize(50)
-                  }
-                });
-              }}
-            />
+            <Fragment>
+              <Directions
+                origin={region}
+                destination={destination}
+                onReady={result => {
+                  this.mapView.fitToCoordinates(result.coordinates, {
+                    edgePadding: {
+                      right: getPixelSize(50),
+                      left: getPixelSize(50),
+                      top: getPixelSize(50),
+                      bottom: getPixelSize(50)
+                    }
+                  });
+                }}
+              />
+              <Marker
+                coordinate={destination}
+                anchor={{ x: 0, y: 0 }}
+                image={markerImage}
+              />
+            </Fragment>
           )}
         </MapView>
         <Search onLocationSelected={this.handleLocationSelected} />
