d03499e97f1e32d525f74fc619add6c7cbf0ea4d
Douglas Porto <douglasalexandre7@gmail.com>
2019-06-18 18:48:55 -0300

    InputText search

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import MapView from 'react-native-maps';
+import { getPixelSize } from '../../utils';
 
+import Search from '../Search';
+import Directions from '../Directions';
 export default class Map extends Component {
   state = {
-    region: null
+    region: null,
+    destination: null
   };
   async componentDidMount() {
     navigator.geolocation.getCurrentPosition(
@@ -26,8 +30,20 @@ export default class Map extends Component {
       }
     );
   }
+  handleLocationSelected = (data, { geometry }) => {
+    const {
+      location: { lat: latitude, lng: longitude }
+    } = geometry;
+    this.setState({
+      destination: {
+        latitude,
+        longitude,
+        title: data.structured_formatting.main_text
+      }
+    });
+  };
   render() {
-    const { region } = this.state;
+    const { region, destination } = this.state;
     return (
       <View style={{ flex: 1 }}>
         <MapView
@@ -35,7 +51,26 @@ export default class Map extends Component {
           region={region}
           showsUserLocation
           loadingEnabled
-        />
+          ref={el => (this.mapView = el)}
+        >
+          {destination && (
+            <Directions
+              origin={region}
+              destination={destination}
+              onReady={result => {
+                this.mapView.fitToCoordinates(result.coordinates, {
+                  edgePadding: {
+                    right: getPixelSize(50),
+                    left: getPixelSize(50),
+                    top: getPixelSize(50),
+                    bottom: getPixelSize(50)
+                  }
+                });
+              }}
+            />
+          )}
+        </MapView>
+        <Search onLocationSelected={this.handleLocationSelected} />
       </View>
     );
   }
---
+++ src/components/Search/index.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { Platform } from 'react-native';
+export default class Search extends Component {
+  state = {
+    searchFocused: false
+  };
+  render() {
+    const { searchFocused } = this.state;
+    const { onLocationSelected } = this.props;
+    return (
+      <GooglePlacesAutocomplete
+        placeholder="Para onde?"
+        placeholderTextColor="#333"
+        onPress={onLocationSelected}
+        query={{
+          key: 'AIzaSyBxLPBCilS_zmFRpDGMuzdMk66THz9W21I',
+          language: 'pt'
+        }}
+        textInputProps={{
+          onFocus: () => {
+            this.setState({ searchFocused: true });
+          },
+          onBlur: () => {
+            this.setState({ searchFocused: false });
+          },
+          autoCapitalize: 'none',
+          autoCorrect: false
+        }}
+        listViewDisplayed={searchFocused}
+        fetchDetails
+        enablePoweredByContainer={false}
+        styles={{
+          container: {
+            position: 'absolute',
+            top: Platform.select({ ios: 60, android: 40 }),
+            width: '100%'
+          },
+          textInputContainer: {
+            flex: 1,
+            backgroundColor: 'transparent',
+            height: 54,
+            marginHorizontal: 20,
+            borderTopWidth: 0,
+            borderBottomWidth: 0
+          },
+          textInput: {
+            height: 54,
+            margin: 0,
+            borderRadius: 0,
+            paddingTop: 0,
+            paddingBottom: 0,
+            paddingLeft: 20,
+            paddingRight: 20,
+            marginTop: 0,
+            marginBottom: 0,
+            marginLeft: 0,
+            marginRight: 0,
+            elevation: 5,
+            shadowColor: '#000',
+            shadowOpacity: 0.1,
+            shadowOffset: { x: 0, y: 0 },
+            shadowRadius: 15,
+            borderWidth: 1,
+            borderColor: '#DDD',
+            fontSize: 18
+          },
+          listView: {
+            borderWidth: 1,
+            borderColor: '#DDD',
+            backgroundColor: '#FFF',
+            marginHorizontal: 20,
+            elevation: 5,
+            shadowOpacity: 0.1,
+            shadowOffset: { x: 0, y: 0 },
+            shadowRadius: 15,
+            marginTop: 10
+          },
+          description: {
+            fontSize: 16
+          },
+          row: {
+            padding: 20,
+            height: 58
+          }
+        }}
+      />
+    );
+  }
+}
