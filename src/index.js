b9b8f48b2c198dbdba06c5b647d5aa389e28e0db
Douglas Porto <douglasalexandre7@gmail.com>
2019-06-17 21:36:07 -0300

    To put on maps

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import MapView from 'react-native-maps';
 
 import { View } from 'react-native';
 
 // import { Container } from './styles';
 
-const App = () => <View />;
+const App = () => (
+  <View style={{ flex: 1 }}>
+    <MapView
+      style={{ flex: 1 }}
+      region={{
+        latitude: -27.210753,
+        longitude: -49.644183,
+        latitudeDelta: 0.0143,
+        longitudeDelta: 0.0134
+      }}
+      showsUserLocation
+      loadingEnabled
+    />
+  </View>
+);
 
 export default App;
